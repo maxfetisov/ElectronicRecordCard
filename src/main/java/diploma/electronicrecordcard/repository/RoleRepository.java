ef587b18b2562d02ee40b1c37190269ecb0785a2
maxfetisov <maksimka.fetisov.02@mail.ru>
2024-04-20 21:49:26 +0300

    Получение ролей

---
+++ src/main/java/diploma/electronicrecordcard/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package diploma.electronicrecordcard.repository;
+
+import diploma.electronicrecordcard.data.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Short> {
+}
---
+++ src/main/java/diploma/electronicrecordcard/service/impl/RoleServiceImpl.java
@@ -0,0 +1,31 @@
+package diploma.electronicrecordcard.service.impl;
+
+import diploma.electronicrecordcard.data.dto.model.RoleDto;
+import diploma.electronicrecordcard.data.entity.Role;
+import diploma.electronicrecordcard.repository.RoleRepository;
+import diploma.electronicrecordcard.service.RoleService;
+import diploma.electronicrecordcard.service.mapper.Mapper;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+public class RoleServiceImpl implements RoleService {
+
+    RoleRepository roleRepository;
+
+    Mapper<RoleDto, Role> roleMapper;
+
+    @Override
+    public List<RoleDto> getAll() {
+        return roleRepository.findAll().stream()
+                .map(roleMapper::toDto)
+                .toList();
+    }
+
+}
