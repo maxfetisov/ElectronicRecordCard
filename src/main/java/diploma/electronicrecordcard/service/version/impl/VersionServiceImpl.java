a624b4506b807590b04b562b32bd8a974f666fe2
maxfetisov <maksimka.fetisov.02@mail.ru>
2024-05-26 01:21:21 +0300

    Версии для администратора

--- src/main/java/diploma/electronicrecordcard/service/version/impl/VersionServiceImpl.java
+++ src/main/java/diploma/electronicrecordcard/service/version/impl/VersionServiceImpl.java
@@ -1,7 +1,9 @@
 package diploma.electronicrecordcard.service.version.impl;
 
 import diploma.electronicrecordcard.data.Versionable;
+import diploma.electronicrecordcard.data.enumeration.RoleName;
 import diploma.electronicrecordcard.repository.VersionRepository;
+import diploma.electronicrecordcard.service.account.AuthorityService;
 import diploma.electronicrecordcard.service.mapper.Mapper;
 import diploma.electronicrecordcard.service.version.VersionService;
 import lombok.AccessLevel;
@@ -18,8 +20,11 @@ public class VersionServiceImpl<DTO, ENTITY extends Versionable> implements Vers
 
     Mapper<DTO, ENTITY> mapper;
 
+    AuthorityService authorityService;
+
     @Override
     public List<DTO> getByVersion(Long version) {
+        authorityService.checkRolesAndThrow(List.of(RoleName.ADMINISTRATOR));
         return repository.findByVersionGreaterThan(version).stream()
                 .map(mapper::toDto)
                 .toList();
